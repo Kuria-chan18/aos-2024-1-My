8ac53ad93eb33e1ebac90586aaa19203f27c7ec7
Maria Clara de Araújo Monteiro <113060036+Kuria-chan18@users.noreply.github.com>
2024-04-14 22:46:39 +0000

    Deploy

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,29 +1,35 @@
-import "dotenv/config";
-import cors from "cors";
-import express from "express";
-import { models } from "./models";
-import { routes } from "./routes";
+//import "dotenv/config";
+//import cors from "cors";
+//import express from "express";
+//import { models } from "./models";
+//import { routes } from "./routes";
 
-const app = express();
+//const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-});
+//app.use(cors());
+//app.use(express.json());
+//pp.use(express.urlencoded({ extended: true }));
+//app.use((req, res, next) => {
+  //req.context = {
+    //models,
+    //me: models.users[1],
+  //};
+  //next();
+//});
 
-app.use("/session", routes.session);
-app.use("/users", routes.user);
-app.use("/messages", routes.message);
+//app.use("/session", routes.session);
+//app.use("/users", routes.user);
+//app.use("/messages", routes.message);
 
-app.get("/", (req, res) => {
-  return res.send("Hello Express!");
-});
+//app.get("/", (req, res) => {
+  //return res.send("Hello Express!");
+//});
+
+//const port = process.env.PORT || 3000;
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const dbUrl = process.env.DB_URL;
+const apiKey = process.env.API_KEY;
